14f0dce48517d5280a53cac492e2bcbacf3cc3f6
XADE <notadevel@gmail.com>
2023-12-12 00:58:44 +0530

    service worker to take immediate action

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,5 @@
 self.addEventListener('install', function(event) {
+  self.skipWaiting(); // Skip waiting and activate immediately
   event.waitUntil(
     caches.open('snake-maze').then(function(cache) {
       return cache.addAll([
@@ -14,6 +15,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  self.clients.claim(); // Take control of pages immediately
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
