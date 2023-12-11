150e80f4834e962fd1efd94322e01d488d4d08e2
XADE <notadevel@gmail.com>
2023-12-12 01:36:03 +0530

    Consider handling events like 'controllerchange' to ensure that you wait for the service worker to become active

--- static/activateSw.js
+++ static/activateSw.js
@@ -1,13 +1,12 @@
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/static/service-worker.js')
     .then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
+
+      // Trigger the installation prompt from your application code
+      registration.active.postMessage({ action: 'installApp' });
     })
     .catch(function(error) {
       console.error('Service Worker registration failed:', error);
     });
-
-  // Trigger the installation prompt from your application code
-  navigator.serviceWorker.controller.postMessage({ action: 'installApp' });
 }
