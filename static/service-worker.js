8180d7f79e1c540465b31bf5599ca2e2efc667bc
XADE <notadevel@gmail.com>
2023-12-12 00:32:24 +0530

    test pwa

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,21 +1,12 @@
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('student-panel').then(function(cache) {
+    caches.open('snake-maze').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',  
         '/script.js',
         '/style.css',
       ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
     })
   );
 });
