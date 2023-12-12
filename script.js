4af6ebff66786e3cc785bea1bf7952d6efd1300e
XADE <notadevel@gmail.com>
2023-12-12 14:13:20 +0530

    fp & modularity

--- script.js
+++ script.js
@@ -1,70 +1,53 @@
-// Define HTML elements
-const board = document.getElementById("game-board");
-const instructionText = document.getElementById("instruction-text");
-const score = document.getElementById("score");
-const highScoreText = document.getElementById("highScore");
-const link = document.getElementById("extLink");
+// Functional utilities
+const getElement = (id) => document.getElementById(id);
 
-// Define game variables
-const gridSize = 20;
-let snake = [{ x: 10, y: 10 }];
-let food = generateFood();
-let highScore = 0;
-let direction = "right";
-let gameInterval;
-let gameSpeedDelay = 200;
-let gameStarted = false;
+const createGameElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  return element;
+};
 
-// Draw game map, snake, food
-function draw() {
-  board.textContent = "";
-  drawSnake();
-  drawFood();
-  updateScore();
-}
+const setPosition = (element, position) => {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+};
 
-// Draw snake
-function drawSnake() {
+const generateRandomCoordinate = (gridSize) => Math.floor(Math.random() * gridSize) + 1;
+
+const generateFood = (gridSize) => ({ x: generateRandomCoordinate(gridSize), y: generateRandomCoordinate(gridSize) });
+
+const drawSnake = (board, snake) => {
   snake.forEach((segment) => {
     const snakeElement = createGameElement("div", "snake");
     setPosition(snakeElement, segment);
     board.appendChild(snakeElement);
   });
-}
+};
 
-// Create a snake or food cube/div
-function createGameElement(tag, className) {
-  const element = document.createElement(tag);
-  element.classList.add(className);
-  return element;
-}
-
-// Set the position of snake or food
-function setPosition(element, position) {
-  element.style.gridColumn = position.x;
-  element.style.gridRow = position.y;
-}
-
-// Draw food function
-function drawFood() {
+const drawFood = (board, food, gameStarted) => {
   if (gameStarted) {
     const foodElement = createGameElement("div", "food");
     setPosition(foodElement, food);
     board.appendChild(foodElement);
   }
-}
+};
 
-// Generate food
-function generateFood() {
-  const x = Math.floor(Math.random() * gridSize) + 1;
-  const y = Math.floor(Math.random() * gridSize) + 1;
-  return { x, y };
-}
+const updateScore = (scoreElement, snake) => {
+  const currentScore = snake.length - 1;
+  scoreElement.textContent = currentScore.toString().padStart(3, "0");
+};
 
-// Moving the snake
-function move() {
-  const head = { ...snake[0] };
-  switch (direction) {
+const draw = (gameState) => {
+  gameState.board.textContent = "";
+  drawSnake(gameState.board, gameState.snake);
+  drawFood(gameState.board, gameState.food, gameState.gameStarted);
+  updateScore(gameState.score, gameState.snake);
+};
+
+// Game functions
+const move = (gameState) => {
+  const head = { ...gameState.snake[0] };
+  switch (gameState.direction) {
     case "up":
       head.y--;
       break;
@@ -79,107 +62,105 @@ function move() {
       break;
   }
 
-  snake.unshift(head);
+  gameState.snake.unshift(head);
 
-  if (head.x === food.x && head.y === food.y) {
-    food = generateFood();
-    increaseSpeed();
-    clearInterval(gameInterval); // Clear past interval
-    gameInterval = setInterval(() => {
-      move();
-      checkCollision();
-      draw();
-    }, gameSpeedDelay);
+  if (head.x === gameState.food.x && head.y === gameState.food.y) {
+    gameState.food = generateFood(gameState.gridSize);
+    increaseSpeed(gameState);
+    clearInterval(gameState.gameInterval); // Clear past interval
+    gameState.gameInterval = setInterval(() => {
+      move(gameState);
+      checkCollision(gameState);
+      draw(gameState);
+    }, gameState.gameSpeedDelay);
   } else {
-    snake.pop();
+    gameState.snake.pop();
   }
-}
+};
 
-// Start game function
-function startGame() {
-  gameStarted = true; // Keep track of a running game
-  instructionText.style.display = "none";
-  link.style.display = "none";
-  gameInterval = setInterval(() => {
-    move();
-    checkCollision();
-    draw();
-  }, gameSpeedDelay);
-}
-
-function increaseSpeed() {
-  if (gameSpeedDelay > 25) {
-    gameSpeedDelay -=
-      gameSpeedDelay > 150
+const increaseSpeed = (gameState) => {
+  if (gameState.gameSpeedDelay > 25) {
+    gameState.gameSpeedDelay -=
+      gameState.gameSpeedDelay > 150
         ? 5
-        : gameSpeedDelay > 100
+        : gameState.gameSpeedDelay > 100
           ? 3
-          : gameSpeedDelay > 50
+          : gameState.gameSpeedDelay > 50
             ? 2
             : 1;
   }
-}
+};
 
-function checkCollision() {
-  const head = snake[0];
+const resetGame = (gameState) => {
+  updateHighScore(gameState);
+  stopGame(gameState);
+  gameState.snake = [{ x: 10, y: 10 }];
+  gameState.food = generateFood(gameState.gridSize);
+  gameState.direction = "right";
+  gameState.gameSpeedDelay = 200;
+  updateScore(gameState.score, gameState.snake);
+};
 
-  if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
-    resetGame();
+const stopGame = (gameState) => {
+  clearInterval(gameState.gameInterval);
+  gameState.gameStarted = false;
+  gameState.instructionText.style.display = "block";
+  gameState.link.style.display = "block";
+};
+
+const updateHighScore = (gameState) => {
+  const currentScore = gameState.snake.length - 1;
+  if (currentScore > gameState.highScore) {
+    gameState.highScore = currentScore;
+    gameState.highScoreText.textContent = gameState.highScore.toString().padStart(3, "0");
   }
+  gameState.highScoreText.style.display = "block";
+};
 
-  for (let i = 1; i < snake.length; i++) {
-    if (head.x === snake[i].x && head.y === snake[i].y) {
-      resetGame();
+const isOppositeDirection = (dir1, dir2) => (
+  (dir1 === "up" && dir2 === "down") ||
+  (dir1 === "down" && dir2 === "up") ||
+  (dir1 === "left" && dir2 === "right") ||
+  (dir1 === "right" && dir2 === "left")
+);
+
+// Event listener for pointerdown, pointermove, and pointerup
+const handleInput = (gameState, event) => {
+  let newDirection;
+  if (
+    !gameState.gameStarted &&
+    ((event.type === "pointerdown" && isClickInsideBoard(event, gameState)) ||
+      (event.type === "keydown" && event.key === " "))
+  ) {
+    startGame(gameState);
+  } else if (gameState.gameStarted) {
+    switch (event.type) {
+      case "keydown":
+        newDirection = getDirectionFromKey(event.key);
+        break;
+      case "pointerdown":
+        if (isClickInsideBoard(event, gameState)) {
+          gameState.isDragging = true;
+          setDragStart(event, gameState);
+        }
+        break;
+      case "pointermove":
+        if (gameState.isDragging) {
+          newDirection = setDirectionFromDrag(event, gameState);
+        }
+        break;
+      case "pointerup":
+        gameState.isDragging = false;
+        break;
+    }
+    if (newDirection !== undefined && !isOppositeDirection(newDirection, gameState.direction)) {
+      gameState.direction = newDirection;
+      return;
     }
   }
-}
+};
 
-function resetGame() {
-  updateHighScore();
-  stopGame();
-  snake = [{ x: 10, y: 10 }];
-  food = generateFood();
-  direction = "right";
-  gameSpeedDelay = 200;
-  updateScore();
-}
-
-function updateScore() {
-  const currentScore = snake.length - 1;
-  score.textContent = currentScore.toString().padStart(3, "0");
-}
-
-function stopGame() {
-  clearInterval(gameInterval);
-  gameStarted = false;
-  instructionText.style.display = "block";
-  link.style.display = "block";
-}
-
-function updateHighScore() {
-  const currentScore = snake.length - 1;
-  if (currentScore > highScore) {
-    highScore = currentScore;
-    highScoreText.textContent = highScore.toString().padStart(3, "0");
-  }
-  highScoreText.style.display = "block";
-}
-
-function isOppositeDirection(dir1, dir2) {
-  return (
-    (dir1 === "up" && dir2 === "down") ||
-    (dir1 === "down" && dir2 === "up") ||
-    (dir1 === "left" && dir2 === "right") ||
-    (dir1 === "right" && dir2 === "left")
-  );
-}
-
-// Variables to track input during dragging
-let isDragging = false;
-let dragStartX = 0;
-let dragStartY = 0;
-
-function getDirectionFromKey(key) {
+const getDirectionFromKey = (key) => {
   switch (key) {
     case "ArrowUp":
       return "up";
@@ -190,25 +171,25 @@ function getDirectionFromKey(key) {
     case "ArrowRight":
       return "right";
     default:
-      return direction;
+      return gameState.direction;
   }
-}
+};
 
-function setDragStart(event) {
-  dragStartX = event.clientX;
-  dragStartY = event.clientY;
-}
+const setDragStart = (event, gameState) => {
+  gameState.dragStartX = event.clientX;
+  gameState.dragStartY = event.clientY;
+};
 
-function isClickInsideBoard(event) {
-  const rect = board.getBoundingClientRect();
+const isClickInsideBoard = (event, gameState) => {
+  const rect = gameState.board.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
   return x >= 0 && x <= rect.width && y >= 0 && y <= rect.height;
-}
+};
 
-function setDirectionFromDrag(event) {
-  const deltaX = event.clientX - dragStartX;
-  const deltaY = event.clientY - dragStartY;
+const setDirectionFromDrag = (event, gameState) => {
+  const deltaX = event.clientX - gameState.dragStartX;
+  const deltaY = event.clientY - gameState.dragStartY;
   return Math.abs(deltaX) > Math.abs(deltaY)
     ? deltaX > 0
       ? "right"
@@ -216,46 +197,59 @@ function setDirectionFromDrag(event) {
     : deltaY > 0
     ? "down"
     : "up";
-}
+};
 
-// Event listener for pointerdown, pointermove, and pointerup
-function handleInput(event) {
-  let newDirection;
-  if (
-    !gameStarted &&
-    ((event.type === "pointerdown" && isClickInsideBoard(event)) ||
-      (event.type === "keydown" && event.key === " "))
-  ) {
-    startGame();
-  } else if (gameStarted) {
-    switch (event.type) {
-      case "keydown":
-        newDirection = getDirectionFromKey(event.key);
-        break;
-      case "pointerdown":
-        if (isClickInsideBoard(event)) {
-          isDragging = true;
-          setDragStart(event);
-        }
-        break;
-      case "pointermove":
-        if (isDragging) {
-          newDirection = setDirectionFromDrag(event);
-        }
-        break;
-      case "pointerup":
-        isDragging = false;
-        break;
-    }
-    if (newDirection !== undefined && !isOppositeDirection(newDirection, direction)) {
-      // Ignore opposite direction
-      direction = newDirection;
-      return;
+// Game functions
+const startGame = (gameState) => {
+  gameState.gameStarted = true;
+  gameState.instructionText.style.display = "none";
+  gameState.link.style.display = "none";
+  gameState.gameInterval = setInterval(() => {
+    move(gameState);
+    checkCollision(gameState);
+    draw(gameState);
+  }, gameState.gameSpeedDelay);
+};
+
+// Game functions
+const checkCollision = (gameState) => {
+  const head = gameState.snake[0];
+
+  if (head.x < 1 || head.x > gameState.gridSize || head.y < 1 || head.y > gameState.gridSize) {
+    resetGame(gameState);
+  }
+
+  for (let i = 1; i < gameState.snake.length; i++) {
+    if (head.x === gameState.snake[i].x && head.y === gameState.snake[i].y) {
+      resetGame(gameState);
     }
   }
-}
+};
 
-document.addEventListener("keydown", handleInput);
-document.addEventListener("pointerdown", handleInput);
-document.addEventListener("pointermove", handleInput);
-document.addEventListener("pointerup", handleInput);
+// Initial game state
+const gameState = {
+  board: getElement("game-board"),
+  instructionText: getElement("instruction-text"),
+  score: getElement("score"),
+  highScoreText: getElement("highScore"),
+  link: getElement("extLink"),
+  gridSize: 20,
+  snake: [{ x: 10, y: 10 }],
+  food: generateFood(20),
+  highScore: 0,
+  direction: "right",
+  gameInterval: null,
+  gameSpeedDelay: 200,
+  gameStarted: false,
+  isDragging: false,
+  dragStartX: 0,
+  dragStartY: 0,
+};
+
+// Event listeners
+document.addEventListener("keydown", handleInput.bind(null, gameState));
+document.addEventListener("pointerdown", handleInput.bind(null, gameState));
+document.addEventListener("pointermove", handleInput.bind(null, gameState));
+document.addEventListener("pointerup", handleInput.bind(null, gameState));
+
+// Add any additional event listeners or initialization code as needed.
